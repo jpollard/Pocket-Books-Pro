97aae2bb2698e0a33423404d0c3aca3f8f980bfb
Joseph Pollard <joseph.w.pollard@gmail.com>
2015-02-13 11:09:27 -0500

    Merge branch 'actionbar' of https://github.com/jpollard/Pocket-Books-Pro into actionbar
    Conflicts:
    	res/layout-v11/overview_activity_layout.xml
    	res/values/styles.xml
    	src/com/happyrobotics/pocketbookspro/AccountAdapter.java
    	src/com/happyrobotics/pocketbookspro/AccountsActivity.java
    	src/com/happyrobotics/pocketbookspro/OverviewActivity.java
    	src/com/happyrobotics/pocketbookspro/TransactionActivity.java
    Cleaning up some of the legacy code

---
+++ app/src/main/java/com/happyrobotics/pocketbookspro/Prefs.java
@@ -0,0 +1,21 @@
+package com.happyrobotics.pocketbookspro;
+
+
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Prefs extends PreferenceActivity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+		
+	}
+	
+	
+	
+	
+
+}
